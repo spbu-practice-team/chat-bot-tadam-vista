5408b6daaf0a24cfe0cf818663ac8e4148911cb9
Victor <61326288+kolbacer@users.noreply.github.com>
2021-11-05 21:20:13 +0300

    Update FieldInfo.py
    inheritaned from BaseModel
    smth with class attributes idk

--- youtrack_api/schemas/FieldInfo.py
+++ youtrack_api/schemas/FieldInfo.py
@@ -1,11 +1,7 @@
-class FieldInfo:
-    def __init__(self, name, value, is_valid=True):
-        self.name = name  # always string
-        self.value = value  # not always string
-        self.is_valid = is_valid
+from typing import Any
+from pydantic import BaseModel
 
-    def show(self):
-        if self.is_valid:
-            print(f"{self.name}: {self.value}")
-        else:
-            print("Not valid")
+
+class FieldInfo(BaseModel):
+    name: str
+    value: Any
